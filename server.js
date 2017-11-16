f6305c2be461ae7f142c5f5b1ca95be096c6a8c9
Michael Sadahiro <michaelsadahiro@gmail.com>
2017-11-15 20:56:14 -0800

    changed some files on server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express()
 var bodyParser = require('body-parser')
 var path = require('path')
 
-app.use(express.static(__dirname, 'public'))
-app.use(express.static(__dirname, 'node_modules'))
+app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname, './node_modules')))
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
